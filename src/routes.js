75b1b03a3224961908b2c04b31df9c9f667e0339
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2021-11-25 21:22:51 -0300

    request and response of pages

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = express.Router()
+
+const basePath = __dirname + "/views"
+
+//request, response 
+routes.get('/', (request, response) =>   response.sendFile(basePath + "/index.html"))
+routes.get('/job', (request, response) =>   response.sendFile(basePath + "/job.html"))
+routes.get('/job/edit', (request, response) =>   response.sendFile(basePath + "/job-edit.html"))
+routes.get('/profile', (request, response) =>   response.sendFile(basePath + "/profile.html"))
+
+
+
+module.exports = routes;
+// 3:18:42
--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 const { request } = require("express")
 const express = require("express")
 const server = express()
+const routes = require('./routes')
 
 // Habilitar arquivos statics
 server.use(express.static("public"))
 
-
-//request, response 
-server.get('/', (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
+// routes
+server.use(routes)
 
 server.listen(3000, () => console.log('rodando'))
-// 2:55:00
