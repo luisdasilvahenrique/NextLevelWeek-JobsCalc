8dbff4e87b66d35614d738e24c316618bf348062
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2021-12-08 13:46:03 -0300

    refactoring create and save in:routes.js

--- src/routes.js
+++ src/routes.js
@@ -13,79 +13,93 @@ const profile = {
   "value-hour": 75,
 };
 
-const jobs = [
-  {
-    id: 1,
-    name: "Pizzaria Guloso",
-    "daily-hours": 2,
-    "total-hours": 60,
-    created_at: Date.now(),
-  },
-  {
-    id: 2,
-    name: "Onetwo Project",
-    "daily-hours": 4,
-    "total-hours": 35,
-    created_at: Date.now(),
-  },
-];
+const Job = {
+  data: [
+    {
+      id: 1,
+      name: "Pizzaria Guloso",
+      "daily-hours": 2,
+      "total-hours": 1,
+      created_at: Date.now(),
+    },
+    {
+      id: 2,
+      name: "Onetwo Project",
+      "daily-hours": 4,
+      "total-hours": 35,
+      created_at: Date.now(),
+    },
+  ],
 
-function remainingDays(job) {
-  // ajustes no job
-  // calculo de tempo restante
-  const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+  controllers: {
+    index(req, res) {
+        const updateJobs = Job.data.map((job) => {
+          //ajustes no job
+          const remaining = Job.services.remainingDays(job);
+          const status = remaining <= 0 ? "done" : "progress";
+      
+          return {
+            ...job,
+            remaining,
+            status,
+            budget: (profile["value-hour"] = job["total-hours"]),
+          };
+        });
+      
+        return res.render(views + "index", { jobs: updateJobs })
+    },
 
-  const createdDate = new Date(job.created_at);
-  const dueDay = createdDate.getDate() + Number(remainingDays);
-  const dueDateInMs = createdDate.setDate(dueDay);
+    create(req, res){
+      return res.render(views, "job")
+    },
 
-  const timeDiffInsMs = dueDateInMs - Date.now();
-  // transformar milissegundos em dias
-  const dayInMs = 1000 * 60 * 60 * 24;
-  const dayDiff = Math.floor(timeDiffInsMs / dayInMs);
+    save(req, res){
+      const lastId = Job.data[Job.data.length - 1]?.id || 1;
 
-  // restam ox dias
-  return dayDiff;
-}
+      jobs.push({//aqui antes era jobs
+        // estou mandando isto para o const jobs = []
+        id: lastId + 1,
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() // atribuindo data de hoje
+      })
+      return res.redirect("/")
+        }
+    },  
+
+    services: {
+      remainingDays(job) {
+        // ajustes no job
+        // calculo de tempo restante
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+      
+        const createdDate = new Date(job.created_at);
+        const dueDay = createdDate.getDate() + Number(remainingDays);
+        const dueDateInMs = createdDate.setDate(dueDay);
+      
+        const timeDiffInsMs = dueDateInMs - Date.now();
+        // transformar milissegundos em dias
+        const dayInMs = 1000 * 60 * 60 * 24;
+        const dayDiff = Math.floor(timeDiffInsMs / dayInMs);
+      
+        // restam ox dias
+        return dayDiff
+      }
+    }
+  }
+
+//3:05
+
 
 //req, res
-routes.get("/", (req, res) => {
+routes.get("/", Job.controllers.index)
 
-
-  const updateJobs = jobs.map((job) => {
-    //ajustes no job
-    const remaining = remainingDays(job);
-    const status = remaining <= 0 ? "done" : "progress";
-
-    return {
-      ...job,
-      remaining,
-      status,
-      budget: (profile["value-hour"] = job["total-hours"]),
-    };
-  });
-
-  return res.render(views + "index", { jobs: updateJobs });
-});
-routes.get("/job", (req, res) => res.render(views + "job"));
-routes.post("/job", (req, res) => {
-  // req.body{ name: 'asdf', 'daily-hours': '3', 'total-hours': '12' }
-
-  const lastId = jobs[jobs.length - 1]?.id || 1;
-
-  jobs.push({
-    // estou mandando isto para o const jobs = []
-    id: lastId + 1,
-    name: req.body.name,
-    "daily-hours": req.body["daily-hours"],
-    "total-hours": req.body["total-hours"],
-    created_at: Date.now(), // atribuindo data de hoje
-  });
-  return res.redirect("/");
-});
-routes.get("/job/edit", (req, res) => res.render(views + "job-edit"));
+routes.get("/job", Job.controllers.create)
+routes.post("/job", Job.controllers.save)
+routes.get("/job/edit", (req, res) => res.render(views + "job-edit"))
 routes.get("/profile", (req, res) =>
   res.render(views + "profile", { profile })
-);
+  )
 
 module.exports = routes;
